2070144330476d2f05f3b9df48ae034cf498d70b
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-10-14 11:32:45 +0100

    first testing function added

---
+++ tests/renderAlphabet.test.js
@@ -0,0 +1,33 @@
+describe("renderAlphabet", () => {
+    it("Render the alphabet correctly in the div buttons", () => {
+      
+      document.body.innerHTML = `
+        <section class="buttons">
+          <div class="buttons__button" id="1"></div>
+          <div class="buttons__button" id="2"></div>
+          <div class="buttons__button" id="3"></div>
+          <div class="buttons__button" id="4"></div>
+          <div class="buttons__button" id="5"></div>
+          <div class="buttons__button" id="6"></div>
+          <div class="buttons__button" id="7"></div>
+        </section>`;
+
+      const mockDataToRender = ["a","b","c","d","e","f","g"];
+  
+      const buttons = document.querySelectorAll(".buttons__button");
+      
+      renderAlphabet(buttons, mockDataToRender);
+      
+      
+      expect(document.body.innerHTML).toBe(`
+        <section class="buttons">
+          <div class="buttons__button" id="1">a</div>
+          <div class="buttons__button" id="2">b</div>
+          <div class="buttons__button" id="3">c</div>
+          <div class="buttons__button" id="4">d</div>
+          <div class="buttons__button" id="5">e</div>
+          <div class="buttons__button" id="6">f</div>
+          <div class="buttons__button" id="7">g</div>
+        </section>`);
+    });
+});
